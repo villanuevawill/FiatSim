d0de029c470bce89bfa8289a9f8806a16cec1685
mihai6 <mihai@element.fi>
2022-05-18 19:15:37 -0400

    initial

--- charts.py
+++ charts.py
@@ -4,20 +4,20 @@ import numpy as np
 import itertools
 import json
 
-f = open('fiatsim.json')
+# f = open('fiatsim.json')
 f1 = open('fiatsim_flashloan.json')
 
-data = json.load(f)
+# data = json.load(f)
 data_flash = json.load(f1)
 
-x = [x["result"]["startingDaiBalance"] for x in data]
-y = [(y["result"]["finalAPY"] * 100) for y in data]
+# x = [x["result"]["startingDaiBalance"] for x in data]
+# y = [(y["result"]["finalAPY"] * 100) for y in data]
 
-plt.plot(x, y)
-plt.title('APR per DAI')
-plt.xlabel('Starting Dai Balance')
-plt.ylabel('APR %')
-plt.show()
+# plt.plot(x, y)
+# plt.title('APR per DAI')
+# plt.xlabel('Starting Dai Balance')
+# plt.ylabel('APR %')
+# plt.show()
 
 x1 = [x["result"]["startingDaiBalance"] for x in data_flash]
 y1 = [(y["result"]["finalAPY"] * 100) for y in data_flash]
@@ -28,33 +28,33 @@ plt.xlabel('Starting Dai Balance')
 plt.ylabel('APR %')
 plt.show()
 
-max_value = max(y)
-max_index = y.index(max_value)
+# max_value = max(y)
+# max_index = y.index(max_value)
 
-max_result = data[max_index]["result"]
-df = pd.DataFrame([max_result])
-print(df.to_markdown())
-df.to_html('max_result.html')
+# max_result = data[max_index]["result"]
+# df = pd.DataFrame([max_result])
+# print(df.to_markdown())
+# df.to_html('max_result.html')
 
-results = [x["result"] for x in data]
+# results = [x["result"] for x in data]
 flash_loan_results = [x["result"] for x in data_flash]
 
-cycles_grouping = [x["cycles"] for x in data]
-cycles = list(itertools.chain.from_iterable(cycles_grouping))
+# cycles_grouping = [x["cycles"] for x in data]
+# cycles = list(itertools.chain.from_iterable(cycles_grouping))
 
-aggregates_grouping = [x["aggregateCycles"] for x in data]
-aggregates = list(itertools.chain.from_iterable(aggregates_grouping))
+# aggregates_grouping = [x["aggregateCycles"] for x in data]
+# aggregates = list(itertools.chain.from_iterable(aggregates_grouping))
 
-df = pd.DataFrame(results)
-df.to_html('results.html')
+# df = pd.DataFrame(results)
+# df.to_html('results.html')
 
-df1 = pd.DataFrame(cycles)
-df1.to_html('cycles.html')
+# df1 = pd.DataFrame(cycles)
+# df1.to_html('cycles.html')
 
-df2 = pd.DataFrame(aggregates)
-df2.to_html('aggregates.html')
+# df2 = pd.DataFrame(aggregates)
+# df2.to_html('aggregates.html')
 
 df3 = pd.DataFrame(flash_loan_results)
 df3.to_html('flasloan_results.html')
 
-f.close()
+# f.close()
